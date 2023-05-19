b1b7ec4548d3bb73e70002a04121bb6f33a506eb
Albin Lindeborg <albin.lindeborg@gmail.com>
2023-05-19 16:22:17 +0200

    add button to remove picker

--- src/components/Picker.tsx
+++ src/components/Picker.tsx
@@ -1,7 +1,21 @@
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import { logos, tradesApi } from '../util/config'
 import { Player } from '../types'
-import { logos } from '../util/config'
 
-const Picker = (props: { picker: string, players: Player[] }) => {	
+const Picker = (props: { picker: string, players: Player[] }) => {
+	const removePicker = (id: number) => {
+		fetch(`${tradesApi}/players/${id}`, {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({})
+		})
+			.then(res => res.json())
+			.then(data => console.log(data))
+			.catch(error => console.error(error))
+	}
+
 	return (
 		<div className='col mt-5'>
 			<h3>{props.picker}</h3>
@@ -9,15 +23,12 @@ const Picker = (props: { picker: string, players: Player[] }) => {
 			{props.players
 				.sort((a, b) => a.team - b.team)
 				.map((player, index) => (
-					<div
-						key={index}
-						className='mb-1'
-					>
+					<div key={index} className='mb-1'>
 						<img
 							src={`${logos}/${player.team}.svg`}
 							alt={player.name}
 						/>
-						{player.name}
+						{player.name} <i className='bi bi-x-circle' onClick={() => removePicker(player.id)}></i>
 					</div>
 				))
 			}
